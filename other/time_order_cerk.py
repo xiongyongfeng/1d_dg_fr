5c8d9badfcc025f64c96cd8627669a6023271d34
xyf <yongfeng.xiong@outlook.com>
2025-12-30 00:24:37 +0800

    update

--- other/time_order_cerk.py
+++ other/time_order_cerk.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 定义参数
-lambda_val = -3  # λ值
+lambda_val = -2.01  # λ值
 y0 = 1.0  # 初始条件 y(0)=1
 t0 = 0.0  # 初始时间
 T = 1.0  # 评估时间点
@@ -154,6 +154,63 @@ def cerk4(f, y0, t0, T, h):
     return un, t_seq, y_seq
 
 
+def cerk4_dimaxer(f, y0, t0, T, h):
+    b01 = 1
+    b02 = 0
+    b03 = 0
+    b04 = 0
+    b11 = -65 / 48
+    b12 = 529 / 384
+    b13 = 125 / 128
+    b14 = -1
+    b21 = 41 / 72
+    b22 = -529 / 576
+    b23 = -125 / 192
+    b24 = 1
+    a21 = 12 / 23
+    a31 = -68 / 375
+    a32 = 368 / 375
+    a41 = 31 / 144
+    a42 = 529 / 1154  # dimaxer 529/1154  #paper 529/1152
+    a43 = 125 / 384
+
+    t = t0
+    un = y0
+    t_seq = []
+    y_seq = []
+    t_seq.append(t)
+    y_seq.append(un)
+    n = int(round((T - t0) / h))  # 计算步数
+    for i in range(n):
+        v1 = un
+        k1 = f(t, v1)
+        v2 = un + a21 * k1 * h
+        k2 = f(t, v2)
+        v3 = un + (a31 * k1 + a32 * k2) * h
+        k3 = f(t, v3)
+        v4 = un + (a41 * k1 + a42 * k2 + a43 * k3) * h
+        k4 = f(t, v4)
+        # c0 = b01 * k1 + b02 * k2 + b03 * k3 + b04 * k4
+        # c1 = (b11 * k1 + b12 * k2 + b13 * k3 + b14 * k4) / h
+        # c2 = (b21 * k1 + b22 * k2 + b23 * k3 + b24 * k4) / h / h
+
+        # un = un + c0 * h + c1 * h * h + c2 * h * h * h
+        # 等价于
+        # un = un + h * (
+        #     (b01 + b11 + b21) * k1
+        #     + (b02 + b12 + b22) * k2
+        #     + (b03 + b13 + b23) * k3
+        #     + (b04 + b14 + b24) * k4
+        # )  # paper
+        un = un + 1 / 24 * k1 + 23 / 24 * k2  # dimaxer
+        t = t + h
+        t_seq.append(t)
+        y_seq.append(un)
+        # if h == 0.025:
+        #     print(f"t={t},y(t)={un}")
+    return un, t_seq, y_seq
+
+
 def cerk6(f, y0, t0, T, h):
     b01 = 1
     b02 = 0
@@ -252,107 +309,104 @@ def cerk6(f, y0, t0, T, h):
     return un, t_seq, y_seq
 
 
-def twosteptwostage(f, y0, t0, T, h):
-    omega = 1 / 1.0
-    theta = 1.0
+def cerk6_dimaxer(f, y0, t0, T, h):
+    b01 = 1
+    b02 = 0
+    b03 = 0
+    b04 = 0
+    b05 = 0
+    b06 = 0
+    b11 = -104217 / 37466
+    b12 = 0
+    b13 = 861101 / 230560
+    b14 = -63869 / 293440
+    b15 = -1522125 / 762944
+    b16 = 165 / 131
+    b21 = 1806901 / 618189
+    b22 = 0
+    b23 = -2178079 / 380424
+    b24 = 6244423 / 5325936
+    b25 = 982125 / 190736
+    b26 = -461 / 131
+    b31 = -866577 / 824252
+    b32 = 0
+    b33 = 12308679 / 5072320
+    b34 = -7816583 / 10144640
+    b35 = -624375 / 217984
+    b36 = 296 / 131
+    a21 = 1 / 6
+    a31 = 44 / 1369
+    a32 = 363 / 1369
+    a41 = 3388 / 4913
+    a42 = -8349 / 4913
+    a43 = 8140 / 4913
+    a51 = -36764 / 408375
+    a52 = 767 / 1125
+    a53 = -32708 / 136125
+    a54 = 210392 / 408375
+    a61 = 1697 / 18876  # dimaxer 1697 / 18876   #paper -1697 / 18876
+    a62 = 0
+    a63 = 50653 / 116160
+    a64 = 299693 / 1626240
+    a65 = 3375 / 11648
 
-    a_20 = 0
-    d_20 = ((2.0 - np.sqrt(2.0)) / (2.0 * omega)) ** 2
-    v2 = 2.0
-
-    # a_20 = 0
-    # d_20 = (3.0 / 2.0 / omega) ** 2
-    # v2 = 0.2
-
-    # a_20 = -1
-    # d_20 = 1
-    # v2 = 0.167
-
-    a_21 = omega * (np.sqrt(d_20 - 2 * a_20) + d_20 - a_20)
-    d_21 = 1.0 - d_20
-    b0 = (
-        2 * theta**3
-        + 3 * theta**2 * omega
-        - 6 * v2 * omega * omega * (np.sqrt(d_20 - 2 * a_20) + d_20 - 2 * a_20)
-    ) / omega**3
-    b1 = 1 - b0
-
-    v0 = (
-        theta**3
-        + theta**2 * omega
-        - v2 * omega * omega * (3 * (d_20 - 2 * a_20) + 2 * np.sqrt(d_20 - 2 * a_20))
-    ) / omega**3
-    v1 = theta * (theta + omega) ** 2 / omega**2 - v2 * (
-        1 + 4 * np.sqrt(d_20 - 2 * a_20) + 3 * (d_20 - 2 * a_20)
-    )
-
-    t = t0
-    n = int(round((T - t0) / h))  # 计算步数
-    u_n = y0
-    k1_0 = lambda_y(t0, y0)
-    k2_0 = lambda_y(t0, y0 - k1_0 * h)
-    u_n_1 = y0 - 0.5 * h * (k1_0 + k2_0)
-    # u_n_1 = y0  # 第一步会严重影响精度
-    # u_n_1 = y0 - lambda_y(t0, y0) * h
-    # u_n_1 = exact_solution(-h)
+    print(f"(b01 + b11 + b21 + b31)={b01 + b11 + b21 + b31}")
+    print(f"(b02 + b12 + b22 + b32)={b02 + b12 + b22 + b32}")
+    print(f"(b03 + b13 + b23 + b33)={b03 + b13 + b23 + b33}")
+    print(f"(b04 + b14 + b24 + b34)={b04 + b14 + b24 + b34}")
+    print(f"(b05 + b15 + b25 + b35)={b05 + b15 + b25 + b35}")
+    print(f"(b06 + b16 + b26 + b36)={b06 + b16 + b26 + b36}")
 
     t_seq = []
     y_seq = []
+
+    t = t0
+    un = y0
     t_seq.append(t)
-    y_seq.append(u_n)
-
-    for i in range(n):
-        k0 = lambda_y(t, u_n_1)
-        # if i == 0:
-        #     k0 = 0
-        k1 = lambda_y(t, u_n)
-        y2 = d_20 * u_n_1 + d_21 * u_n + h * (omega * a_20 * k0 + a_21 * k1)
-        k2 = lambda_y(t, y2)
-        y = b0 * u_n_1 + b1 * u_n + h * (omega * v0 * k0 + v1 * k1 + v2 * k2)
-        t = t + h
-        u_n_1 = u_n
-        u_n = y
-        t_seq.append(t)
-        y_seq.append(y)
-    return y, t_seq, y_seq
-
-
-# 欧拉向前方法计算在时间T的数值解
-def new_timescheme(f, y0, t0, T, h):
-    w = 0.6  # only for new timesceme
+    y_seq.append(un)
     n = int(round((T - t0) / h))  # 计算步数
-    t_0 = t0
-    t_1 = t0 + h
-    y_tmp_0 = y0 - h * f(t0, y0)
-    y_tmp_1 = y0
-    ustar_0 = y0
-    ustar_1 = ustar_0 + h * f(t0, y0)
-    print(f"ustar_0={ustar_0},ustar_1={ustar_1},f(ustar_0, t_0)={f(ustar_0, t_0)}")
     for i in range(n):
-        # print(
-        #     f"i={i},t_0 = {t_0},t_1 = {t_1},y_tmp_0={y_tmp_0},y_tmp_1={y_tmp_1},ustar_0={ustar_0},ustar_1={ustar_1}"
-        # )
-        # y_0 = (
-        #     (1.0 / w - 1) * y_tmp_0
-        #     + (2 - 1.0 / w) * y_tmp_1
-        #     + 0.5 * h * (y_tmp_0 + (2.0 / w - 3.0) * y_tmp_1)
-        # )
-        # y_1 = y_tmp_1 + 0.5 * h * (-y_tmp_0 + 3 * y_tmp_1)
-
-        y_0 = (
-            (1.0 / w - 1) * y_tmp_0
-            + (2 - 1.0 / w) * y_tmp_1
-            + 0.5 * h * ((2.0 / w - 1.0) * f(t_0, ustar_0) - f(t_1, ustar_1))
+        v1 = un
+        k1 = f(t, v1)
+        v2 = un + a21 * k1 * h
+        k2 = f(t, v2)
+        v3 = un + (a31 * k1 + a32 * k2) * h
+        k3 = f(t, v3)
+        v4 = un + (a41 * k1 + a42 * k2 + a43 * k3) * h
+        k4 = f(t, v4)
+        v5 = un + (a51 * k1 + a52 * k2 + a53 * k3 + a54 * k4) * h
+        k5 = f(t, v5)
+        v6 = un + (a61 * k1 + a62 * k2 + a63 * k3 + a64 * k4 + a65 * k5) * h
+        k6 = f(t, v6)
+        c0 = b01 * k1 + b02 * k2 + b03 * k3 + b04 * k4 + b05 * k5 + b06 * k6
+        c1 = (b11 * k1 + b12 * k2 + b13 * k3 + b14 * k4 + b15 * k5 + b16 * k6) / h
+        c2 = (b21 * k1 + b22 * k2 + b23 * k3 + b24 * k4 + b25 * k5 + b26 * k6) / h / h
+        c3 = (
+            (b31 * k1 + b32 * k2 + b33 * k3 + b34 * k4 + b35 * k5 + b36 * k6)
+            / h
+            / h
+            / h
         )
-        y_1 = y_tmp_1 + 0.5 * h * (f(t_0, ustar_0) + f(t_1, ustar_1))
-        y_tmp_0 = y_0
-        y_tmp_1 = y_1
-        ustar_0 = y_tmp_1
-        ustar_1 = -y_tmp_0 + 2 * y_tmp_1
-        t_0 = t_0 + h
-        t_1 = t_1 + h
-    y = y_1
-    return y
+
+        # un = un + c0 * h + c1 * h * h + c2 * h * h * h + c3 * h * h * h * h
+        # 等价于
+        # un = un + h * (
+        #     (b01 + b11 + b21 + b31) * k1
+        #     + (b02 + b12 + b22 + b32) * k2
+        #     + (b03 + b13 + b23 + b33) * k3
+        #     + (b04 + b14 + b24 + b34) * k4
+        #     + (b05 + b15 + b25 + b35) * k5
+        #     + (b06 + b16 + b26 + b36) * k6
+        # )  # paper
+
+        un = un + h * (
+            101 / 363 * k1 - 1369 / 14520 * k3 + 11849 / 14520 * k4
+        )  # dimaxer
+
+        t = t + h
+        t_seq.append(t)
+        y_seq.append(un)
+    return un, t_seq, y_seq
 
 
 def time_order_analysis(time_scheme):
@@ -368,7 +422,7 @@ def time_order_analysis(time_scheme):
         error = abs(y_numerical - y_exact)
         errors.append(error)
 
-    log_errors = np.log(errors)
+    log_errors = np.log10(errors)
 
     index = 0
     order = 0
@@ -454,127 +508,15 @@ def cerk4_continuous(f, y0, t0, T, h):
     return t_last_time_step_seq, y_last_time_step_seq
 
 
-def twosteptwostage_continuous(y0, t0, T, h):
-    omega = 1 / 1.0
-    theta = 1.0
-
-    a_20 = 0
-    d_20 = ((2.0 - np.sqrt(2.0)) / (2.0 * omega)) ** 2
-    v2 = 2.0
-
-    # a_20 = 0
-    # d_20 = (3.0 / 2.0 / omega) ** 2
-    # v2 = 0.2
-
-    # a_20 = -1
-    # d_20 = 1
-    # v2 = 0.167
-
-    a_21 = omega * (np.sqrt(d_20 - 2 * a_20) + d_20 - a_20)
-    d_21 = 1.0 - d_20
-    b0 = (
-        2 * theta**3
-        + 3 * theta**2 * omega
-        - 6 * v2 * omega * omega * (np.sqrt(d_20 - 2 * a_20) + d_20 - 2 * a_20)
-    ) / omega**3
-    b1 = 1 - b0
-
-    v0 = (
-        theta**3
-        + theta**2 * omega
-        - v2 * omega * omega * (3 * (d_20 - 2 * a_20) + 2 * np.sqrt(d_20 - 2 * a_20))
-    ) / omega**3
-    v1 = theta * (theta + omega) ** 2 / omega**2 - v2 * (
-        1 + 4 * np.sqrt(d_20 - 2 * a_20) + 3 * (d_20 - 2 * a_20)
-    )
-
-    t = t0
-    n = int(round((T - t0) / h))  # 计算步数
-    u_n = y0
-    # u_n_1 = y0  # 第一步会严重影响精度
-    u_n_1 = exact_solution(-h)
-    # u_n_1 = y0 - lambda_y(t0, y0) * h
-
-    t_last_time_step_seq = []
-    y_last_time_step_seq = []
-    t_seq = []
-    y_seq = []
-    t_seq.append(t)
-    y_seq.append(u_n)
-
-    for i in range(n):
-        y2 = (
-            d_20 * u_n_1
-            + d_21 * u_n
-            + h * (omega * a_20 * lambda_y(t, u_n_1) + a_21 * lambda_y(t, u_n))
-        )
-        y = (
-            b0 * u_n_1
-            + b1 * u_n
-            + h
-            * (
-                omega * v0 * lambda_y(t, u_n_1)
-                + v1 * lambda_y(t, u_n)
-                + v2 * lambda_y(t, y2)
-            )
-        )
-
-        if i == n - 1:
-            ratio = np.linspace(0, 1, 101)
-            for r in ratio:
-                b0 = (
-                    2 * r**3
-                    + 3 * r**2 * omega
-                    - 6
-                    * v2
-                    * omega
-                    * omega
-                    * (np.sqrt(d_20 - 2 * a_20) + d_20 - 2 * a_20)
-                ) / omega**3
-                b1 = 1 - b0
-
-                v0 = (
-                    r**3
-                    + r**2 * omega
-                    - v2
-                    * omega
-                    * omega
-                    * (3 * (d_20 - 2 * a_20) + 2 * np.sqrt(d_20 - 2 * a_20))
-                ) / omega**3
-                v1 = r * (r + omega) ** 2 / omega**2 - v2 * (
-                    1 + 4 * np.sqrt(d_20 - 2 * a_20) + 3 * (d_20 - 2 * a_20)
-                )
-                t_last_time_step_seq.append(t + h * r)
-                u_r = (
-                    b0 * u_n_1
-                    + b1 * u_n
-                    + h
-                    * (
-                        omega * v0 * lambda_y(t, u_n_1)
-                        + v1 * lambda_y(t, u_n)
-                        + v2 * lambda_y(t, y2)
-                    )
-                )
-                y_last_time_step_seq.append(u_r)
-        t = t + h
-        u_n_1 = u_n
-        u_n = y
-        t_seq.append(t)
-        y_seq.append(y)
-    return t_last_time_step_seq, y_last_time_step_seq
-
-
 def cerk_continous_soln_analysis():
     h = 0.025
     t_seq_cerk4, y_seq_cerk4 = cerk4_continuous(lambda_y, y0, t0, T, h)
-    t_seq_2step2stage, y_seq_2step2stage = twosteptwostage_continuous(y0, t0, T, h)
 
     t_seq = np.linspace(T - h, T, 101)
     y_seq_analysis = exact_solution(t_seq)
 
     plt.figure(figsize=(10, 6))
     plt.plot(t_seq_cerk4, y_seq_cerk4, label="CERK4")
-    plt.plot(t_seq_2step2stage, y_seq_2step2stage, label="2step2stage")
     plt.plot(t_seq, y_seq_analysis, label="exact soln")
     plt.title("continuous soln in one time step")
     plt.xlabel("t")
@@ -586,35 +528,26 @@ def cerk_continous_soln_analysis():
 
 if __name__ == "__main__":
 
-    cerk_continous_soln_analysis()
+    # cerk_continous_soln_analysis()
 
-    log_h = np.log(h_list)
+    log_h = np.log10(h_list)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-
-    # print("New Scheme:")
-    # errors_new_timesceme, log_error_new_timesceme, coefficients_new = (
-    #     time_order_analysis(new_timescheme)
-    # )
-    # plt.plot(
-    #     log_h,
-    #     log_error_new_timesceme,
-    #     "o-",
-    #     label=f"new schme error (slope = {coefficients_new[0]:.4f})",
-    #     markersize=8,
-    # )
 
     print("\nEuler forward:")
     errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(euler_forward)
     ax1.plot(
         log_h,
         log_errors,
-        "o-",
+        "ko-",
         label=f"euler forward error (slope = {coefficients[0]:.4f})",
         markersize=8,
     )
     ax2.plot(
-        t_seq[4], u_seq[4], label=f"euler forward error (slope = {coefficients[0]:.4f})"
+        t_seq[4],
+        u_seq[4],
+        "k-",
+        label=f"euler forward error (slope = {coefficients[0]:.4f})",
     )
 
     print("\n RK4:")
@@ -622,45 +555,103 @@ if __name__ == "__main__":
     ax1.plot(
         log_h,
         log_errors,
-        "o-",
+        "ro-",
         label=f"RK4 (slope = {coefficients[0]:.4f})",
         markersize=8,
     )
-    ax2.plot(t_seq[0], u_seq[0], label=f"RK4 (slope = {coefficients[0]:.4f})")
+    ax2.plot(t_seq[0], u_seq[0], "r-", label=f"RK4 (slope = {coefficients[0]:.4f})")
+
+    print("\n CERK2:")
+    errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(cerk2)
+    ax1.plot(
+        log_h,
+        log_errors,
+        "bo-",
+        label=f"CERK2 (slope = {coefficients[0]:.4f})",
+        markersize=8,
+    )
+    ax2.plot(t_seq[0], u_seq[0], "b-", label=f"CERK2 (slope = {coefficients[0]:.4f})")
 
     print("\n CERK4:")
     errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(cerk4)
     ax1.plot(
         log_h,
         log_errors,
-        "o-",
+        "ob--",
         label=f"CERK4 (slope = {coefficients[0]:.4f})",
         markersize=8,
     )
-    ax2.plot(t_seq[1], u_seq[1], label=f"CERK4 (slope = {coefficients[0]:.4f})")
+    ax2.plot(t_seq[1], u_seq[1], "b--", label=f"CERK4 (slope = {coefficients[0]:.4f})")
 
-    print("\n2step2stage:")
-    errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(
-        twosteptwostage
-    )
+    # CHECK
+    # print("\n CERK4-Dimaxer:")
+    # errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(cerk4_dimaxer)
+    # ax1.plot(
+    #     log_h,
+    #     log_errors,
+    #     "og--",
+    #     label=f"CERK4-Dimaxer (slope = {coefficients[0]:.4f})",
+    #     markersize=8,
+    # )
+    # ax2.plot(
+    #     t_seq[1],
+    #     u_seq[1],
+    #     "g--",
+    #     label=f"CERK4-Dimaxer (slope = {coefficients[0]:.4f})",
+    # )
+
+    print("\n CERK6:")
+    errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(cerk6)
     ax1.plot(
         log_h,
         log_errors,
-        "o-",
-        label=f"2step2stage (slope = {coefficients[0]:.4f})",
+        "ob-.",
+        label=f"CERK6 (slope = {coefficients[0]:.4f})",
         markersize=8,
     )
-    ax2.plot(t_seq[0], u_seq[0], label=f"2step2stage (slope = {coefficients[0]:.4f})")
+    ax2.plot(t_seq[0], u_seq[0], "b-.", label=f"CERK6 (slope = {coefficients[0]:.4f})")
+
+    print("\n CERK6-Dimaxer:")
+    errors, log_errors, coefficients, t_seq, u_seq = time_order_analysis(cerk6_dimaxer)
+    ax1.plot(
+        log_h,
+        log_errors,
+        "go-.",
+        label=f"CERK6-Dimaxer (slope = {coefficients[0]:.4f})",
+        markersize=8,
+    )
+    ax2.plot(
+        t_seq[0],
+        u_seq[0],
+        "g-.",
+        label=f"CERK6-Dimaxer (slope = {coefficients[0]:.4f})",
+    )
 
     ax1.set_xlabel("log(h)")
     ax1.set_ylabel("log(error)")
     ax1.set_title("(log-log)")
-    ax1.legend()
+    # ax1.legend()
+    # ax1.set_aspect(1)
     ax1.grid(True, which="both", ls="--")
 
     ax2.set_xlabel("t")
     ax2.set_ylabel("y(t)")
     ax2.set_title("y-t")
-    ax2.legend()
+    # ax2.legend()
     ax2.grid(True, which="both", ls="--")
-    plt.savefig("time_order.png", dpi=300)
+    ax1.set_aspect(0.1, adjustable="box")
+
+    fig.legend(
+        loc="lower center",
+        bbox_to_anchor=(0.5, 0),  # 在图形正下方
+        ncol=4,  # 4列布局
+        fontsize=9,
+        frameon=True,
+        framealpha=0.95,
+        edgecolor="gray",
+        title="legend",
+        title_fontsize=10,
+    )
+    # plt.tight_layout()
+    plt.tight_layout(rect=[0, 0.25, 1, 1])
+    plt.savefig("time_order_cerk.png", dpi=300)
